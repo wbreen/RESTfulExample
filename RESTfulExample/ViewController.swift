c1e7b6381ab94748835e19df7d7f929786ab519a
William Breen <wbreen@mooney202-17.elon.edu>
2016-11-02 14:20:02 -0400

    just started restful example

---
+++ RESTfulExample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  RESTfulExample
+//
+//  Created by William Breen on 11/2/16.
+//  Copyright © 2016 William Breen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func doRandomize(_ sender: UIButton) {
+    }
+
+
+}
+
